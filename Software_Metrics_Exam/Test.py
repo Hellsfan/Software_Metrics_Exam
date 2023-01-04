a7680fc0fc7b99fa6c4a9a494cbea662e9105bf0
Filip Petkov <filip55555@abv.bg>
2023-01-04 11:34:00 +0200

    Added Tests for different type of input

--- Software_Metrics_Exam/Test.py
+++ Software_Metrics_Exam/Test.py
@@ -1,13 +1,33 @@
+from asyncio.windows_events import NULL
 import unittest
 from Software_Metrics_Exam import absolute
 
 class TestAbsoluteMethods(unittest.TestCase):
 
-    def test_does_it_work(self):
+    def test_equal(self):
         self.assertEqual(absolute(-5),5)
         self.assertEqual(absolute(5),5)
+
+    def test_not_equal(self):
         self.assertNotEqual(absolute(-10), -10)
         self.assertNotEqual(absolute(10), -10)
+
+    def test_input_str(self):
+       input_str="-5";
+       with self.assertRaises(TypeError):
+            absolute(input_str)
+
+    def test_input_blank(self):
+        input_blank=""
+        with self.assertRaises(TypeError):
+            absolute(input_blank)
+    
+    def test_input_null(self):
+        input_null=NULL
+        with self.assertRaises(TypeError):
+            absolute(input_null)
+
+    def test_test(self):
 
 if __name__ == '__main__':
     unittest.main()
