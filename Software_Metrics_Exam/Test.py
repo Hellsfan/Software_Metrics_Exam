808a9749b5edb897cfac0eb684a1767920b27cd3
Filip Petkov <filip55555@abv.bg>
2023-01-04 11:24:50 +0200

    First Test case

--- Software_Metrics_Exam/Test.py
+++ Software_Metrics_Exam/Test.py
@@ -4,7 +4,10 @@ from Software_Metrics_Exam import absolute
 class TestAbsoluteMethods(unittest.TestCase):
 
     def test_does_it_work(self):
-        pass;
+        self.assertEqual(absolute(-5),5)
+        self.assertEqual(absolute(5),5)
+        self.assertNotEqual(absolute(-10), -10)
+        self.assertNotEqual(absolute(10), -10)
 
 if __name__ == '__main__':
     unittest.main()
