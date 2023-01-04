0b2972700a6e7ff6066d57a86014920ef10b7bee
Filip Petkov <filip55555@abv.bg>
2023-01-04 11:22:38 +0200

    Definition unittest initial

--- Software_Metrics_Exam/Test.py
+++ Software_Metrics_Exam/Test.py
@@ -1,1 +1,10 @@
+import unittest
+from Software_Metrics_Exam import absolute
 
+class TestAbsoluteMethods(unittest.TestCase):
+
+    def test_does_it_work(self):
+        pass;
+
+if __name__ == '__main__':
+    unittest.main()
